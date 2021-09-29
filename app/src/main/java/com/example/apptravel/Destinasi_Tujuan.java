9aeb4a289ce908aa755409a5475f9a8857b536d6
Rani Fanika <ranifanika09@gmail.com>
2021-09-30 06:30:02 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/apptravel/Destinasi_Tujuan.java
@@ -0,0 +1,14 @@
+package com.example.apptravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Destinasi_Tujuan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destinasi_tujuan);
+    }
+}
---
+++ app/src/main/java/com/example/apptravel/Pilih_hotel.java
@@ -0,0 +1,14 @@
+package com.example.apptravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pilih_hotel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_hotel);
+    }
+}
